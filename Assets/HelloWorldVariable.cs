da2c17c7abba88181c0e39d18c6b0d98e45e45f2
James Macbox1 <ninjachimpstudios@gmail.com>
2017-01-26 16:02:53 +1000

    initial

---
+++ Assets/HelloWorldVariable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorldVariable : MonoBehaviour {
+
+	private string _helloStr = "Hello Variable World!";
+	public string YourName;
+	
+	// Use this for initialization
+	void Start () {
+		Debug.Log(_helloStr);
+		Debug.Log("Your name is " + YourName);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
